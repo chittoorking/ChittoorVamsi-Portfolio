0ed179b22c2e7d257763f9e333bf1ed5179c6f71
chittoorking <chittoorvamsi@gmail.com>
2022-08-18 22:28:04 +0530

    skills

--- src/Skills.js
+++ src/Skills.js
@@ -74,7 +74,7 @@ export default function Skills() {
                         <div>
                             <Image
                                 alt="Skills"
-                                src="/skill.svg"
+                                src="/my_data_science_skills.png"
                                 width="1139"
                                 height="655"
                             />
