cc1c9be6779a39031b1efa58dafb6293c9308e5a
chittoorking <chittoorvamsi@gmail.com>
2023-07-02 00:11:21 +0530

    "commit"

--- src/Experience.js
+++ src/Experience.js
@@ -72,7 +72,7 @@ export default function Experience() {
                     Experience
                 </Typography>
                 <Hidden mdDown>
-                    <Fade in={animate} style={{ transitionDelay: '100ms' }}>
+                    <Fade in={animate} style={{ transitionDelay: '10ms' }}>
                         <div>
                             <Image
                                 alt="Experience"
